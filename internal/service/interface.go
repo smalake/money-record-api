a1f1a30f2655b43302a444e85f99c9818ab6a8be
smalake <41889150+smalake@users.noreply.github.com>
2023-12-31 23:44:19 +0900

    メソッド名を変更

--- internal/service/interface.go
+++ internal/service/interface.go
@@ -5,29 +5,29 @@ import "github.com/labstack/echo/v4"
 type ServerInterface interface {
 
 	// (POST /auth-code)
-	AuthCode(ctx echo.Context) error
+	AuthCodeHandler(ctx echo.Context) error
 
 	// (GET /health-check)
-	HealthCheck(ctx echo.Context) error
+	HealthCheckHandler(ctx echo.Context) error
 
 	// (GET /login-check)
-	LoginCheck(ctx echo.Context) error
+	LoginCheckHandler(ctx echo.Context) error
 	// Googleアカウントでログイン
 	// (POST /login-google)
-	LoginGoogle(ctx echo.Context) error
+	LoginGoogleHandler(ctx echo.Context) error
 	// メールアドレスでログイン
 	// (POST /login-mail)
-	LoginMail(ctx echo.Context) error
+	LoginMailHandler(ctx echo.Context) error
 	// ログアウト処理
 	// (GET /logout)
-	Logout(ctx echo.Context) error
+	LogoutHandler(ctx echo.Context) error
 
 	// (POST /register)
-	RegisterUser(ctx echo.Context) error
+	RegisterUserHandler(ctx echo.Context) error
 
 	// (GET /resend-code)
-	ResendCode(ctx echo.Context) error
+	ResendCodeHandler(ctx echo.Context) error
 
 	// (POST /send-mail)
-	SendMail(ctx echo.Context) error
+	SendMailHandler(ctx echo.Context) error
 }
