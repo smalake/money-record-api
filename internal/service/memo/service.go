a0551fa99a62990ae55510fb29c3d04b10e33c3e
smalake <koizumim1002@gmail.com>
2024-03-30 14:19:48 +0900

    memoパッケージのエラーログの表記を修正

--- internal/service/memo/service.go
+++ internal/service/memo/service.go
@@ -19,6 +19,7 @@ func New(am *appmodel.AppModel) *Service {
 func (s *Service) GetMemoAll(ctx echo.Context) structs.HttpResponse {
 	r := new(memos.CreateMemoRequest)
 	if err := ctx.Bind(r); err != nil {
+		ctx.Logger().Errorf("[FATAL] failed to bind: %+v", err)
 		return structs.HttpResponse{Code: 400, Error: err}
 	}
 	uid := ctx.Get("uid")
@@ -27,7 +28,7 @@ func (s *Service) GetMemoAll(ctx echo.Context) structs.HttpResponse {
 	var memo memos.GetMemoResponse
 	err := s.appModel.MysqlCli.DB.Select(&memo, query, uid)
 	if err != nil {
-		ctx.Logger().Errorf("[FATAL] %v", err)
+		ctx.Logger().Errorf("[FATAL] failed to get all memos: %+v", err)
 		return structs.HttpResponse{Code: 500, Error: err}
 	}
 	return structs.HttpResponse{Code: 200, Data: memo}
@@ -41,7 +42,7 @@ func (s *Service) GetMemoOne(ctx echo.Context) structs.HttpResponse {
 	var memo memos.OneMemo
 	err := s.appModel.MysqlCli.DB.Get(&memo, query, id, uid)
 	if err != nil {
-		ctx.Logger().Errorf("[FATAL] %v", err)
+		ctx.Logger().Errorf("[FATAL] failed to get one memo: %+v", err)
 		return structs.HttpResponse{Code: 500, Error: err}
 	}
 	return structs.HttpResponse{Code: 200, Data: memo}
@@ -50,13 +51,14 @@ func (s *Service) GetMemoOne(ctx echo.Context) structs.HttpResponse {
 func (s *Service) CreateMemo(ctx echo.Context) structs.HttpResponse {
 	r := new(memos.OneMemo)
 	if err := ctx.Bind(r); err != nil {
+		ctx.Logger().Errorf("[FATAL] failed to bind: %+v", err)
 		return structs.HttpResponse{Code: 400, Error: err}
 	}
 	uid := ctx.Get("uid")
 	query := mysql.CreateMemo
 	_, err := s.appModel.MysqlCli.DB.Exec(query, uid, r.Amount, r.Partner, r.Memo, r.Date, r.Period, r.Type)
 	if err != nil {
-		ctx.Logger().Errorf("[FATAL] %v", err)
+		ctx.Logger().Errorf("[FATAL] failed to create new memo: %+v", err)
 		return structs.HttpResponse{Code: 500, Error: err, Message: err.Error()}
 	}
 	return structs.HttpResponse{Code: 200}
@@ -65,14 +67,14 @@ func (s *Service) CreateMemo(ctx echo.Context) structs.HttpResponse {
 func (s *Service) UpdateMemo(ctx echo.Context) structs.HttpResponse {
 	r := new(memos.OneMemo)
 	if err := ctx.Bind(r); err != nil {
-		ctx.Logger().Errorf("[FATAL] %v", err)
+		ctx.Logger().Errorf("[FATAL] failed to bind: %+v", err)
 		return structs.HttpResponse{Code: 400, Error: err}
 	}
 	uid := ctx.Get("uid")
 	query := mysql.UpdateMemo
 	_, err := s.appModel.MysqlCli.DB.Exec(query, r.Amount, r.Partner, r.Memo, r.Date, r.Period, r.Type, r.ID, uid)
 	if err != nil {
-		ctx.Logger().Errorf("[FATAL] %v", err)
+		ctx.Logger().Errorf("[FATAL] failed to update memo: %+v", err)
 		return structs.HttpResponse{Code: 500, Error: err}
 	}
 	return structs.HttpResponse{Code: 200}
@@ -81,14 +83,14 @@ func (s *Service) UpdateMemo(ctx echo.Context) structs.HttpResponse {
 func (s *Service) DeleteMemo(ctx echo.Context) structs.HttpResponse {
 	r := new(memos.OneMemo)
 	if err := ctx.Bind(r); err != nil {
-		ctx.Logger().Errorf("[FATAL] %v", err)
+		ctx.Logger().Errorf("[FATAL] failed to bind: %+v", err)
 		return structs.HttpResponse{Code: 400, Error: err}
 	}
 	uid := ctx.Get("uid")
 	query := mysql.DeleteMemo
 	_, err := s.appModel.MysqlCli.DB.Exec(query, r.ID, uid)
 	if err != nil {
-		ctx.Logger().Errorf("[FATAL] %v", err)
+		ctx.Logger().Errorf("[FATAL] failed to delete memo %+v", err)
 		return structs.HttpResponse{Code: 500, Error: err}
 	}
 	return structs.HttpResponse{Code: 200}
